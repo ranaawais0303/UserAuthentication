6856e84653b168fb268a49038d0af465b78f40c2
Rana_awais <z.manzoor@pioneerlogics.com>
2023-01-17 23:16:09 -0800

    controlling signup and login screens

--- components/Auth/AuthContent.js
+++ components/Auth/AuthContent.js
@@ -4,6 +4,7 @@ import { Alert, StyleSheet, View } from "react-native";
 import FlatButton from "../ui/FlatButton";
 import AuthForm from "./AuthForm";
 import { Colors } from "../../constants/styles";
+import { useNavigation } from "@react-navigation/native";
 
 function AuthContent({ isLogin, onAuthenticate }) {
   const [credentialsInvalid, setCredentialsInvalid] = useState({
@@ -12,9 +13,15 @@ function AuthContent({ isLogin, onAuthenticate }) {
     confirmEmail: false,
     confirmPassword: false,
   });
+  const navigation = useNavigation();
 
   function switchAuthModeHandler() {
-    // Todo
+    //check if login prop??then log signup because login page is open
+    if (isLogin) {
+      navigation.replace("Signup");
+    } else {
+      navigation.replace("Login");
+    }
   }
 
   function submitHandler(credentials) {
