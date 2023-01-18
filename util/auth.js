1a884dfd00be75d0344d13e6ebdaaad4b585b738
Rana_awais <z.manzoor@pioneerlogics.com>
2023-01-17 23:50:53 -0800

    signup functionality and create user in firebase by using api

--- util/auth.js
+++ util/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //this is taken from our firebse project
 const API_KEY = "AIzaSyB3Lx8FprzwUnkXmFNUGRGpiXtgENH2odc";
-async function createUser(email, password) {
+export async function createUser(email, password) {
   const response = await axios.post(
     //this is the api from firebase auth rest api signup with email/password
     "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
